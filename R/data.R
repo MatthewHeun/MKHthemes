90ff2a42f4dff10ec77e7fb76c3b997ba207461c
MatthewHeun <matthew.heun@me.com>
2026-07-10 11:02:42 +0100

    Working on parameters

---
+++ R/data.R
@@ -0,0 +1,56 @@
+#' Colours for continents
+#'
+#' A string list containing names of colours for continents.
+#'
+#' @format A string list with `r length(continent_colours)` entries.
+#' \describe{
+#' \item{Africa}{The colour for Africa, "orange".}
+#' \item{Asia}{The colour for Asia, "gray".}
+#' \item{Bunkers}{The colour for bunkers, "darkgray".}
+#' \item{Europe}{The colour for Europe, "Europe".}
+#' \item{MidEast, Middle East}{The colour for the Middle East, "cadetblue3".}
+#' \item{NoAmr, North America}{The colour for North America, "red".}
+#' \item{Oceania}{The colour for Oceania, "blue".}
+#' \item{SoCeAmr, South and Central America}{The colour for South and Central America, "violet".}
+#' \item{Africa}{The colour for World, "black".}
+#' }
+#'
+#' @examples
+#' continent_colours
+"continent_colours"
+
+
+#' @format A string list with `r length(continent_linetypes)` entries.
+#' \describe{
+#' \item{Africa}{The linetype for Africa, "solid".}
+#' \item{Asia}{The linetype for Asia, "solid".}
+#' \item{Bunkers}{The linetype for bunkers, "dashed".}
+#' \item{Europe}{The linetype for Europe, "solid".}
+#' \item{MidEast, Middle East}{The linetype for the Middle East, "solid".}
+#' \item{NoAmr, North America}{The linetype for North America, "solid".}
+#' \item{Oceania}{The linetype for Oceania, "solid".}
+#' \item{SoCeAmr, South and Central America}{The linetype for South and Central America, "solid".}
+#' \item{Africa}{The linetype for World, "solid".}
+#' }
+#'
+#' @examples
+#' continent_linetypes
+"continent_linetypes"
+
+
+#' @format A string list with `r length(continent_linewidths)` entries.
+#' \describe{
+#' \item{Africa}{The linewidth for Africa, 0.3.}
+#' \item{Asia}{The linewidth for Asia, 0.3.}
+#' \item{Bunkers}{The linewidth for bunkers, 0.2.}
+#' \item{Europe}{The linewidth for Europe, 0.3.}
+#' \item{MidEast, Middle East}{The linewidth for the Middle East, 0.3.}
+#' \item{NoAmr, North America}{The linewidth for North America, 0.3.}
+#' \item{Oceania}{The linewidth for Oceania, 0.3.}
+#' \item{SoCeAmr, South and Central America}{The linewidth for South and Central America, 0.3.}
+#' \item{Africa}{The linewidth for World, 0.3.}
+#' }
+#'
+#' @examples
+#' continent_linewidths
+"continent_linewidths"
