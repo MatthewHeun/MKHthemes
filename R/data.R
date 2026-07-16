7cc3dbc9954792c9f767f713621655ba12417a1e
MatthewHeun <matthew.heun@me.com>
2026-07-16 20:57:12 +0100

    Added documentation for new colour constants.

--- R/data.R
+++ R/data.R
@@ -1,6 +1,15 @@
 #' Colours for continents
 #'
 #' A string list containing names of colours for continents.
+#' These colours were used for Figure B1 of
+#' P. E. Brockway, M. K. Heun, Z. Marshall, E. Aramendia,
+#' P. Steenwyk, T. Relph, M. Widjanarko,
+#' J. J. Kim, A. Sainju, and J. Irtube.
+#' A country-level primary-final-useful (CL-PFU) energy and exergy
+#' database: Overview of its construction and 1971–2020
+#' world-level efficiency results.
+#' Environmental Research: Energy, 1(025005):1–29, June 2024.
+#' <https://doi.org/10.1088/2753-3751/ad4e39>
 #'
 #' @format A string list with `r length(continent_colours)` entries.
 #' \describe{
@@ -20,6 +29,19 @@
 "continent_colours"
 
 
+#' Continent linetypes
+#'
+#' A string list containing linetypes for continents.
+#' These linetypes were used for Figure B1 of
+#' P. E. Brockway, M. K. Heun, Z. Marshall, E. Aramendia,
+#' P. Steenwyk, T. Relph, M. Widjanarko,
+#' J. J. Kim, A. Sainju, and J. Irtube.
+#' A country-level primary-final-useful (CL-PFU) energy and exergy
+#' database: Overview of its construction and 1971–2020
+#' world-level efficiency results.
+#' Environmental Research: Energy, 1(025005):1–29, June 2024.
+#' <https://doi.org/10.1088/2753-3751/ad4e39>
+#'
 #' @format A string list with `r length(continent_linetypes)` entries.
 #' \describe{
 #' \item{Africa}{The linetype for Africa, "solid".}
@@ -38,6 +60,19 @@
 "continent_linetypes"
 
 
+#' Continent linewidths
+#'
+#' A string list containing linewieths for continents.
+#' These linetypes were used for Figure B1 of
+#' P. E. Brockway, M. K. Heun, Z. Marshall, E. Aramendia,
+#' P. Steenwyk, T. Relph, M. Widjanarko,
+#' J. J. Kim, A. Sainju, and J. Irtube.
+#' A country-level primary-final-useful (CL-PFU) energy and exergy
+#' database: Overview of its construction and 1971–2020
+#' world-level efficiency results.
+#' Environmental Research: Energy, 1(025005):1–29, June 2024.
+#' https://doi.org/10.1088/2753-3751/ad4e39
+#'
 #' @format A string list with `r length(continent_linewidths)` entries.
 #' \describe{
 #' \item{Africa}{The linewidth for Africa, 0.3.}
@@ -54,3 +89,166 @@
 #' @examples
 #' continent_linewidths
 "continent_linewidths"
+
+
+#' Sector colours
+#'
+#' A string list containing colours for economic sectors.
+#' These colours were used for Figure 6 of
+#' P. E. Brockway, M. K. Heun, Z. Marshall, E. Aramendia,
+#' P. Steenwyk, T. Relph, M. Widjanarko,
+#' J. J. Kim, A. Sainju, and J. Irtube.
+#' A country-level primary-final-useful (CL-PFU) energy and exergy
+#' database: Overview of its construction and 1971–2020
+#' world-level efficiency results.
+#' Environmental Research: Energy, 1(025005):1–29, June 2024.
+#' <https://doi.org/10.1088/2753-3751/ad4e39>
+#'
+#' @format A string list with `r length(sector_colours)` entries.
+#' \describe{
+#' \item{AFF; Agriculture, forestry, and fishing; Agriculture, forestry,\\nand fishing}{The colour for the Agriculture, forestry, and fishing sector.}
+#' \item{Res; Residential}{The colour for the Residential sector.}
+#' \item{Ind; Industry}{The colour for the Industry sector.}
+#' \item{CPS; Commercial and public services; Commercial and\\npublic services}{The colour for the Commercial and public services sector.}
+#' \item{Trans; Transport}{The colour for the Transport sector.}
+#' \item{NES}{The colour for the NES sector.}
+#' \item{Final consumption not elsewhere specified; Final consumption not\\nelsewhere specified}{The colour for unspecified sectors.}
+#' \item{NEU; Non-energy use}{The colour for the Non-energy use sector.}
+#' \item{EIOU; Energy industry own use; Energy industry\\nown use}{The colour for the Energy industry own use sector.}
+#' \item{World}{The colour for a World sector.}
+#' }
+#'
+#' @examples
+#' sector_colours
+"sector_colours"
+
+
+
+#' Primary-stage energy carrier colours
+#'
+#' A string list containing colours for primary-stage energy carriers.
+#' These colours were used for Figure 5 of
+#' P. E. Brockway, M. K. Heun, Z. Marshall, E. Aramendia,
+#' P. Steenwyk, T. Relph, M. Widjanarko,
+#' J. J. Kim, A. Sainju, and J. Irtube.
+#' A country-level primary-final-useful (CL-PFU) energy and exergy
+#' database: Overview of its construction and 1971–2020
+#' world-level efficiency results.
+#' Environmental Research: Energy, 1(025005):1–29, June 2024.
+#' <https://doi.org/10.1088/2753-3751/ad4e39>
+#'
+#' @format A string list with `r length(primary_carrier_colours)` entries.
+#' \describe{
+#' \item{BM; Biomass}{The colour for Biomass.}
+#' \item{CCP; Coal and coal products; Coal and coal\\nproducts}{The colour for Coal and coal products.}
+#' \item{OOP; Oil and oil products; Oil and oil\\nproducts}{The colour for Oil and oil products.}
+#' \item{NG; Natural gas}{The colour for Natural gas.}
+#' \item{BF; Biofuels}{The colour for Biofuels.}
+#' \item{Elect; Electricity}{The colour for Electricity.}
+#' \item{Heat}{The colour for Heat.}
+#' \item{Waste}{The colour for Waste.}
+#' }
+#'
+#' @examples
+#' primary_carrier_colours
+"primary_carrier_colours"
+
+
+
+#' Final-stage energy carrier colours
+#'
+#' A string list containing colours for final-stage energy carriers.
+#' These colours were used for Figure 5 of
+#' P. E. Brockway, M. K. Heun, Z. Marshall, E. Aramendia,
+#' P. Steenwyk, T. Relph, M. Widjanarko,
+#' J. J. Kim, A. Sainju, and J. Irtube.
+#' A country-level primary-final-useful (CL-PFU) energy and exergy
+#' database: Overview of its construction and 1971–2020
+#' world-level efficiency results.
+#' Environmental Research: Energy, 1(025005):1–29, June 2024.
+#' <https://doi.org/10.1088/2753-3751/ad4e39>
+#'
+#' @format A string list with `r length(final_carrier_colours)` entries.
+#' \describe{
+#' \item{Feed}{The colour for Feed.}
+#' \item{Food}{The colour for Food.}
+#' \item{CCP; Coal and coal products; Coal and coal\\nproducts}{The colour for Coal and coal products.}
+#' \item{OOP; Oil and oil products; Oil and oil\\nproducts}{The colour for Oil and oil products.}
+#' \item{NG; Natural gas}{The colour for Natural gas.}
+#' \item{BF; Biofuels}{The colour for Biofuels.}
+#' \item{Elect; Electricity}{The colour for Electricity.}
+#' \item{Heat}{The colour for Heat.}
+#' \item{Waste}{The colour for Waste.}
+#' }
+#'
+#' @examples
+#' final_carrier_colours
+"final_carrier_colours"
+
+
+
+#' Primary- and final-stage energy carrier colours
+#'
+#' A string list containing colours for primary- and
+#' final-stage energy carriers.
+#' These colours were used for Figure 5 of
+#' P. E. Brockway, M. K. Heun, Z. Marshall, E. Aramendia,
+#' P. Steenwyk, T. Relph, M. Widjanarko,
+#' J. J. Kim, A. Sainju, and J. Irtube.
+#' A country-level primary-final-useful (CL-PFU) energy and exergy
+#' database: Overview of its construction and 1971–2020
+#' world-level efficiency results.
+#' Environmental Research: Energy, 1(025005):1–29, June 2024.
+#' <https://doi.org/10.1088/2753-3751/ad4e39>
+#'
+#' @format A string list with `r length(primary_and_final_carrier_colours)` entries.
+#' \describe{
+#' \item{BM; Biomass}{The colour for Biomass.}
+#' \item{Feed}{The colour for Feed.}
+#' \item{Food}{The colour for Food.}
+#' \item{CCP; Coal and coal products; Coal and coal\\nproducts}{The colour for Coal and coal products.}
+#' \item{OOP; Oil and oil products; Oil and oil\\nproducts}{The colour for Oil and oil products.}
+#' \item{NG; Natural gas}{The colour for Natural gas.}
+#' \item{BF; Biofuels}{The colour for Biofuels.}
+#' \item{Elect; Electricity}{The colour for Electricity.}
+#' \item{Heat}{The colour for Heat.}
+#' \item{Waste}{The colour for Waste.}
+#' }
+#'
+#' @examples
+#' primary_and_final_carrier_colours
+"primary_and_final_carrier_colours"
+
+
+
+#' Useful-stage energy carrier colours
+#'
+#' A string list containing colours for useful-stage energy carriers.
+#' These colours were used for Figure 5 of
+#' P. E. Brockway, M. K. Heun, Z. Marshall, E. Aramendia,
+#' P. Steenwyk, T. Relph, M. Widjanarko,
+#' J. J. Kim, A. Sainju, and J. Irtube.
+#' A country-level primary-final-useful (CL-PFU) energy and exergy
+#' database: Overview of its construction and 1971–2020
+#' world-level efficiency results.
+#' Environmental Research: Energy, 1(025005):1–29, June 2024.
+#' <https://doi.org/10.1088/2753-3751/ad4e39>
+#'
+#' @format A string list with `r length(useful_carrier_colours)` entries.
+#' \describe{
+#' \item{AnMech; Animal mechanical work; Animal mechanical\\nwork}{The colour for Animal mechanical work.}
+#' \item{HuMech; Human mechanical work; Human mechanical\\nwork}{The colour for human mechanical work.}
+#' \item{MechW; Mechanical work}{The colour for Mechanical work.}
+#' \item{Prop; Propulsion}{The colour for propulsion.}
+#' \item{HPF; High-pressure fluids; High-pressure\\nfluids}{The colour for High-pressure fluids.}
+#' \item{LTH; Low-temperature heat; Low-temperature\\nheat}{The colour for Low-temperature heat.}
+#' \item{MTH; Medium-temperature heat; Medium-temperature\\nheat}{The colour for medium-temperature heat.}
+#' \item{HTH; High-temperature heat; High-temperature\\nheat}{The colour for High-temperature heat.}
+#' \item{LTC; Low-temperature cooling; Low-temperature\\ncooling}{The colour for Low-temperature cooling.}
+#' \item{IP; Information processing}{The colour for Information processing.}
+#' \item{L; Light}{The colour for Light.}
+#' }
+#'
+#' @examples
+#' useful_carrier_colours
+"useful_carrier_colours"
